c7342b6848cbfc85674a9605e711c2160d7b0c73
Lyn Vil <91258211+simplecoderx@users.noreply.github.com>
2023-03-24 09:27:27 +0800

    Created Controller, Set Up API Route and Seeded Table Carousel

---
+++ app/Http/Controllers/Api/CarouselItemsController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\CarouselItems; //import the mode, importing base on file
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CarouselItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CarouselItems::all();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
